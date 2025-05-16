ec15e1add0cce179d70a4340d5c989963f057e27
lloyd <patrickmheintz@gmail.com>
2025-05-15 20:12:13 -0700

    add coinflip for random one song top and bottom

--- settingsrando/settingsrando.js
+++ settingsrando/settingsrando.js
@@ -1,10 +1,26 @@
 var outputJSON = {};
+const topRowSongs = [
+    "CVars.gRandoSettings.StartingEponasSong",
+    "CVars.gRandoSettings.StartingSariasSong",
+    "CVars.gRandoSettings.StartingSongOfStorms",
+    "CVars.gRandoSettings.StartingSongOfTime",
+    "CVars.gRandoSettings.StartingZeldasLullaby",
+    "CVars.gRandoSettings.StartingSunsSong",
+];
+
+const bottomRowSongs = [
+    "CVars.gRandoSettings.StartingBoleroOfFire",
+    "CVars.gRandoSettings.StartingMinuetOfForest",
+    "CVars.gRandoSettings.StartingNocturneOfShadow",
+    "CVars.gRandoSettings.StartingPreludeOfLight",
+    "CVars.gRandoSettings.StartingRequiemOfSpirit",
+    "CVars.gRandoSettings.StartingSerenadeOfWater"
+]
 function renderInputs(obj, parentKey = '') {
     let html = '';
     for (const key in obj) {
         const fullKey = parentKey ? `${parentKey}.${key}` : key;
         const value = obj[key];
-
         
         if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
             if (key !== 'InjectItemCounts' && key !== 'CVars' && key !== 'gEnhancements' && key !== 'ExtraTraps') {
@@ -80,6 +96,48 @@ function downloadJsonAsFile(jsonData, filename) {
 function putSpacesInCamelCase(str) {
     return str.replace(/([a-z])([A-Z])/g, '$1 $2');
 };
+
+function RandomizeSongs() {
+    // Randomize top row songs
+    var shuffleTopSongs = coinToss();
+    const topLength = topRowSongs.length;
+    const topRandomIndex = Math.floor(Math.random() * topLength);
+    topRowSongs.forEach((song, index) => {
+        const element = document.querySelector(`[id="${song}"]`);
+        if (element) {
+            let minmax;
+            if (shuffleTopSongs) {
+                minmax = (index === topRandomIndex) ? "1" : "0";
+            } else {
+                minmax = "0";
+            }
+            element.setAttribute('data-min', minmax);
+            element.setAttribute('data-max', minmax);
+        }
+    });
+
+    // Randomize bottom row songs
+    var shuffleBottomSongs = coinToss();
+    const bottomLength = bottomRowSongs.length;
+    const bottomRandomIndex = Math.floor(Math.random() * bottomLength);
+    bottomRowSongs.forEach((song, index) => {
+        const element = document.querySelector(`[id="${song}"]`);
+        if (element) {
+            let minmax;
+            if (shuffleBottomSongs) {
+                minmax = (index === bottomRandomIndex) ? "1" : "0";
+            } else {
+                minmax = "0";
+            }
+            element.setAttribute('data-min', minmax);
+            element.setAttribute('data-max', minmax);
+        }
+    });
+}
+
+function coinToss() {
+    return Math.floor(Math.random() * 2) == 0;
+}
 
 const deepSearch = (target, value) => {
     if (typeof target === 'object') {
